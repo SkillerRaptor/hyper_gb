8f9a3d2475f987cdcfc50a6dc24a969322079760
SkillerRaptor <skillerraptor@protonmail.com>
2025-10-11 12:26:38 +0200

    feat: add logger

---
+++ core/include/logger.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025-present, SkillerRaptor
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void logger_info(const char *fmt, ...);
+void logger_warn(const char *fmt, ...);
+void logger_err(const char *fmt, ...);
+void logger_dbg(const char *fmt, ...);
+
+#ifdef __cplusplus
+}
+#endif
